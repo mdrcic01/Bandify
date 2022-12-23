c959748f60812c386887082c3783f2e196b78e52
Martin Drcic <martin.drcic1@gmai.com>
2022-12-23 19:44:52 +0100

    add basic User classes dto and service

---
+++ src/main/java/com/java/bandify/service/user/UserService.java
@@ -0,0 +1,4 @@
+package com.java.bandify.service.user;
+
+public class UserService {
+}
---
+++ src/main/java/com/java/bandify/service/user/model/User.java
@@ -0,0 +1,14 @@
+package com.java.bandify.service.user.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String city;
+    private String instrument;
+}
